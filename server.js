13aa8d754c564d5645e8deb0e5c032ba28b5e792
don21001 <152668565+don21001@users.noreply.github.com>
2024-05-28 10:42:42 +0800

    Update server.js

--- server.js
+++ server.js
@@ -451,8 +451,9 @@ app.get('/download-All-pdf', async (req, res) => {
     const browser = await puppeteer.launch({ 
       headless: true,
       args: ['--no-sandbox', '--disable-setuid-sandbox'],
-      executablePath: '"C:\Program Files\Google\Chrome\Application\chrome.exe"' // Specify the correct path to Chrome executable
+      executablePath: 'C:\\Program Files\\Google\\Chrome\\Application\\chrome.exe' // Correctly escaped backslashes
     });
+
     const page = await browser.newPage();
 
     // Set the HTML content of the page
